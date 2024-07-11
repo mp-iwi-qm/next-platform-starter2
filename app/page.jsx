3a8bf4eb4fc1b633176a2df549730ed9e25dfca7
mp-iwi-qm <fanyuki71@gmail.com>
2024-07-11 09:59:54 +0000

    update

--- app/page.jsx
+++ app/page.jsx
@@ -181,7 +181,7 @@ export default function Page() {
             </div>
             <div className="pt-1 px-4 text-xs tracking-[0.4px] h-[20px]"></div>
           </div>
-          <button className="mb-3 btn-outline relative flex flex-row items-center justify-center gap-x-2 py-2.5 px-6 rounded-[6.25rem] text-sm tracking-[.00714em] font-medium border border-gray-500 text-primary-600 dark:border-gray-400 dark:text-primary-200">
+          <button className="mb-8 btn-outline relative flex flex-row items-center justify-center gap-x-2 py-2.5 px-6 rounded-[6.25rem] text-sm tracking-[.00714em] font-medium border border-gray-500 text-primary-600 dark:border-gray-400 dark:text-primary-200">
             <span className="material-symbols-outlined">+</span>職歴を追加する
           </button>
           <div className="w-full pt-12 pb-8 flex justify-center items-center">
@@ -326,7 +326,7 @@ export default function Page() {
           <div className="mb-12 w-full rounded-md bg-gray-100 h-[60px] dark:bg-gray-700 flex  items-center justify-center">
             <p>本人確認ファイルの撮影またはアップロード</p>
           </div>
-          <button className="mb-12 btn-outline relative flex flex-row items-center justify-center gap-x-2 py-2.5 px-6 rounded-[6.25rem] text-sm tracking-[.00714em] font-medium border border-gray-500 text-primary-600 dark:border-gray-400 dark:text-primary-200">
+          <button className="mt-12 mb-12 btn-outline relative flex flex-row items-center justify-center gap-x-2 py-2.5 px-6 rounded-[6.25rem] text-sm tracking-[.00714em] font-medium border border-gray-500 text-primary-600 dark:border-gray-400 dark:text-primary-200">
             <span className="material-symbols-outlined">✓</span>登録する
           </button>
           </>}
