645d16a639bc3523a6a912feac6ff2fe977b08d1
mp-iwi-qm <fanyuki71@gmail.com>
2024-07-11 06:23:38 +0000

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,13 +15,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-[#CAC4D0]">
-                <div className="flex flex-col min-h-screen px-6 sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <div className="grow">{children}</div>
-                    </div>
-                </div>
-            </body>
+            {children}
         </html>
     );
 }
